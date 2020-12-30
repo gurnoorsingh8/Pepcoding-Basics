b8c7af4a6419ab770c1404831db5406b2aa5682c
Gurnoor Singh <52324678+gurnoorsingh8@users.noreply.github.com>
2020-12-31 00:02:31 +0530

    update 1.8

---
+++ Foundation/Arrays/ceilroof.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void ceilroof(vector<int>& arr, int num)
+{
+    int n = arr.length;
+    for(int i = 0; i < n; i++)
+    {
+        if(num > arr[i] && num < arr[i+1])
+        {
+            cout<<arr[i+1];
+            cout<<arr[i];
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int num;
+    cin >> num;
+    ceilroof(arr, num);
+}
